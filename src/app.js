c7f6f1e8f0a687bd770d2f50e1dae0dbaf2dbeaa
piqueen1 <piqueen@gmail.com>
2017-04-10 14:03:34 -0600

    Fixed simulator error, added ESLINT and Babel stuff

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
+import { View } from 'react-native';
 
 const App = () => {
 	return (
 		<Provider store={createStore(reducers)}>
-			<View />
-		}
+			<View>
+				<Header headerText="Tech Stack"/>
+			</View>
 		</Provider>
 	);
 };
 
-export default App; 
+export default App;
+
